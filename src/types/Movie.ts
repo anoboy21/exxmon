83ca23d2ebbb35048e29ed1ca9888d64044811bf
anoboy21 <109586478+anoboy21@users.noreply.github.com>
2022-11-07 13:13:03 +0800

    Update Movie.ts

--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -5,6 +5,38 @@ export type MovieResponse = {
   total_results: number
 }
 
+export type Item = {
+  poster_path: string;
+  overview: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  backdrop_path: string;
+  popularity: number;
+  vote_count: number;
+  vote_average: number;
+
+  // Additional props
+  media_type: "movie" | "tv" | "person";
+
+  // Movie items
+  release_date?: string;
+  original_title?: string;
+  title?: string;
+  adult?: boolean;
+  video?: boolean;
+
+  // TV Show items
+  first_air_date?: string;
+  original_name?: string;
+  origin_country?: string[];
+  name?: string;
+
+  // Person
+  profile_path?: string;
+}
+
+
 export type Movie = {
   adult: boolean
   backdrop_path: string
